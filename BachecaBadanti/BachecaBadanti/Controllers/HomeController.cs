656b519402cada2449a6ae6096ca8728f9d6d22f
Daniele Paoloni <latinoitalia@hotmail.com>
2017-09-20 19:19:39 +0200

    ddl

--- BachecaBadanti/BachecaBadanti/Controllers/HomeController.cs
+++ BachecaBadanti/BachecaBadanti/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +11,43 @@ namespace BachecaBadanti.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        private bakecaEntities db = new bakecaEntities();
+        // GET: Annuncis
+        public  ActionResult Index()
         {
-            return View();
+
+            //var query = await (from p in db.Annunci
+            //                   where p.Phone != null
+            //                   orderby p.DataOra descending
+            //                   select p).Take(200).ToListAsync();
+
+            //ViewBag.Total = query.Count();
+
+            var provincie = db.Annunci.GroupBy(test => test.City)
+             .Select(grp => grp.FirstOrDefault())
+             .Select(x =>
+                     new SelectListItem
+                     {
+                         Value = x.City,
+                         Text = x.City
+                     });
+
+            
+
+                  
+
+
+            var model = new ItalianCity { CityList = provincie };
+
+
+           // return new SelectList(roles, "Value", "Text");
+
+            //return View(await db.Annunci.OrderByDescending(p => p.DataOra).ToListAsync());
+            return View(model);
         }
+
+      
 
         public ActionResult About()
         {
@@ -26,5 +62,14 @@ namespace BachecaBadanti.Controllers
 
             return View();
         }
+    }
+
+    public class ItalianCity
+    {
+        // Display Attribute will appear in the Html.LabelFor
+        [Display(Name = "Provincie")]
+        public string Selezione { get; set; }
+
+        public IEnumerable<SelectListItem> CityList { get; set; }
     }
 }
